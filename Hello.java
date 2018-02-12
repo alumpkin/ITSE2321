fce03b398947cd3d69062f159d10b3e73bd34964
CSAdmin <alumpkin@midland.edu>
2018-02-12 09:19:07 -0600

    initial commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * Hello Class displays the message Hello World
+ * 
+ * @author Adriana Lumpkin
+ * @version 2/12/2018
+ */
+public class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.print("Hello World");
+    }
+}
